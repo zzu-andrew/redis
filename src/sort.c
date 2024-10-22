0a8e5469578036fc2e1cec9549d2b058510e2795
opt-m <cwjmail1913@gmail.com>
2024-10-22 09:55:00 +0800

    Fix get # option in sort command (#13608)
    From 7.4, Redis allows `GET` options in cluster mode when the pattern maps to
    the same slot as the key, but GET # pattern that represents key itself is missed.
    This commit resolves it, bug report #13607.
    
    ---------
    
    Co-authored-by: Yuan Wang <yuan.wang@redis.com>

--- src/sort.c
+++ src/sort.c
@@ -241,8 +241,12 @@ void sortCommandGeneric(client *c, int readonly) {
         } else if (!strcasecmp(c->argv[j]->ptr,"get") && leftargs >= 1) {
             /* If GET is specified with a real pattern, we can't accept it in cluster mode,
              * unless we can make sure the keys formed by the pattern are in the same slot 
-             * as the key to sort. */
-            if (server.cluster_enabled && patternHashSlot(c->argv[j+1]->ptr, sdslen(c->argv[j+1]->ptr)) != getKeySlot(c->argv[1]->ptr)) {
+             * as the key to sort. The pattern # represents the key itself, so just skip
+             * pattern slot check. */
+            if (server.cluster_enabled &&
+                strcmp(c->argv[j+1]->ptr, "#") &&
+                patternHashSlot(c->argv[j+1]->ptr, sdslen(c->argv[j+1]->ptr)) != getKeySlot(c->argv[1]->ptr))
+            {
                 addReplyError(c, "GET option of SORT denied in Cluster mode when "
                               "keys formed by the pattern may be in different slots.");
                 syntax_error++;
